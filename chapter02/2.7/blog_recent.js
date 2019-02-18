797ff891b897d14ec350df720bba30324be31618
changxianglin <281532612@qq.com>
2019-02-18 12:55:24 +0800

    better code

--- chapter02/2.7/blog_recent.js
+++ chapter02/2.7/blog_recent.js
@@ -2,25 +2,35 @@ const http = require('http')
 const fs = require('fs')
 
 http.createServer((req, res) => {
-    if(req.url == '/') {
-        fs.readFile('./titles.json', (err, data) => {
-            if(err) {
-                console.log(err)
-                res.end('Server Error')
-            } else {
-                const titles = JSON.parse(data.toString())
-                fs.readFile('./template.html', (err, data) => {
-                    if(err) {
-                        console.log(err)
-                        res.end('Server Error')
-                    } else {
-                        const tmpl = data.toString()
-                        const html = tmpl.replace('%', titles.join('</li><li>'))
-                        res.writeHead(200, { 'Content-Type': 'text/html'})
-                        res.end(html)
-                    }
-                })
-            }
-        })
-    }
+    getTitles(res)
 }).listen(8000, '127.0.0.1')
+
+function getTitles(res) {
+    fs.readFile('./titles.json', (err, fata) => {
+        if(err) {
+            hadError(err, res)
+        } else {
+            getTemplate(JSON.parse(data.toString()), res)
+        }
+    })
+}
+
+function getTemplate(titles, res) {
+    fs.readFile('./template.html', (err, data) => {
+        if(err) {
+            hadError(err, res)
+        } else {
+            formatHtml(titles, data.toString(), res)
+        }
+    })
+}
+
+function formatHtml(titles, tmpl, res) {
+    const html = tmpl.replace('%', titles.join('</li><li>'))
+    res.end(html)
+}
+
+function hadError(err, res) {
+    console.log(err)
+    res.end('Server Error')
+}
