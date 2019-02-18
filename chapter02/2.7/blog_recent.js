8f134c81314cfc12e0889e6b6dc9698e50cbd3b3
changxianglin <281532612@qq.com>
2019-02-18 13:00:53 +0800

    best code

--- chapter02/2.7/blog_recent.js
+++ chapter02/2.7/blog_recent.js
@@ -6,27 +6,22 @@ http.createServer((req, res) => {
 }).listen(8000, '127.0.0.1')
 
 function getTitles(res) {
-    fs.readFile('./titles.json', (err, fata) => {
-        if(err) {
-            hadError(err, res)
-        } else {
-            getTemplate(JSON.parse(data.toString()), res)
-        }
+    fs.readFile('./titles.json', (err, data) => {
+        if(err) return hadError(err, res)
+        getTemplate(JSON.parse(data.toString()), res)
     })
 }
 
 function getTemplate(titles, res) {
     fs.readFile('./template.html', (err, data) => {
-        if(err) {
-            hadError(err, res)
-        } else {
-            formatHtml(titles, data.toString(), res)
-        }
+        if(err) return hadError(err, res)
+        formatHtml(titles, data.toString(), res)
     })
 }
 
 function formatHtml(titles, tmpl, res) {
     const html = tmpl.replace('%', titles.join('</li><li>'))
+    res.writeHead(200, { 'Content-Type': 'text/html'})
     res.end(html)
 }
 
