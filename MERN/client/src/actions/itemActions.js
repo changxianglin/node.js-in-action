fd66576dc95df77b9d830a4fcf3288328c98f3dd
changxianglin <281532612@qq.com>
2019-02-21 23:36:55 +0800

    add items component

--- MERN/client/src/actions/itemActions.js
+++ MERN/client/src/actions/itemActions.js
@@ -9,3 +9,10 @@ export const getItems = () => {
         type: GET_ITEMS
     }
 }
+
+export const deleteItem = id => {
+    return {
+        type: DELETE_ITEMS,
+        payload: id
+    }
+}
--- MERN/client/src/reducers/itemReducer.js
+++ MERN/client/src/reducers/itemReducer.js
@@ -20,6 +20,11 @@ export default function(state = initalState, action) {
             return {
                 ...state
             }
+        case DELETE_ITEMS:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            }    
         default:
             return state
     }
