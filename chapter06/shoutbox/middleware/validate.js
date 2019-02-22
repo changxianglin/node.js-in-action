a9650130efd833f034b853d7d0de20defb6bff0f
changxianglin <281532612@qq.com>
2019-02-22 23:52:29 +0800

    change some detail

--- chapter06/shoutbox/middleware/validate.js
+++ chapter06/shoutbox/middleware/validate.js
@@ -1,38 +1,39 @@
-function parserField(field) {
+function parseField(field) {
     return field
-        .splice(/\[|\]/)
-        .filter((s) => s)
-}
-
-function getField(req, field) {
-    let val = req.body
+      .split(/\[|\]/)
+      .filter((s) => s);
+  }
+  
+  function getField(req, field) {
+    let val = req.body;
     field.forEach((prop) => {
-        val = val[prop]
-    })
-    return val
-}
-
-exports.required = (field) => {
-    field = parserField(field)
+      val = val[prop];
+    });
+    return val;
+  }
+  
+  exports.required = (field) => {
+    field = parseField(field);
     return (req, res, next) => {
-        if(getField(req, field)) {
-            next()
-        } else {
-            res.error(`${field.join(' ')} is required`)
-            res.redirect('back')
-        }
-    }
-}
-
-exports.lengthAbove = (field, len) => {
-    field = parseField(field)
+      if (getField(req, field)) {
+        next();
+      } else {
+        res.error(`${field.join(' ')} is required`);
+        res.redirect('back');
+      }
+    };
+  };
+  
+  exports.lengthAbove = (field, len) => {
+    field = parseField(field);
     return (req, res, next) => {
-        if(getField(req, field).lenth > len) {
-            next()
-        } else {
-            const fields = field.join(' ')
-            res.error(`${fields} must have more than ${len} characters`)
-            res.redirect('back')
-        }
-    }
-}
+      if (getField(req, field).length > len) {
+        next();
+      } else {
+        const fields = field.join(' ');
+        res.error(`${fields} must have more than ${len} characters`);
+        res.redirect('back');
+      }
+    };
+  };
+  
--- chapter06/shoutbox/routes/entries.js
+++ chapter06/shoutbox/routes/entries.js
@@ -1,45 +1,32 @@
-const Entry = require('../models/entry')
+const Entry = require('../models/entry');
 
 exports.list = (req, res, next) => {
-    Entry.getRange(0, -1, (err, entries) => {
-        if(err) return next(err)
-        res.render('entries', {
-            title: 'Entries',
-            entries: entries
-        })
-    })
-}
-
+  const page = req.page;
+  Entry.getRange(0, -1, (err, entries) => {
+    if (err) return next(err);
+    res.render('entries', {
+      title: 'Entries',
+      entries: entries
+    });
+  });
+};
 
 exports.form = (req, res) => {
-    res.render('post', {title: 'Post'})
-}
+    console.log('form...')
+  res.render('post', { title: 'Post' });
+};
 
 exports.submit = (req, res, next) => {
-    const data = req.body.entry
-
-    if(!data.title) {
-        res.error('Title is required')
-        res.redirect('bakc')
-        return 
-    }
-
-    if(data.title.length < 4) {
-        res.error('Title must be longer than 4 characters')
-        res.redirect('back')
-        return 
-    }
-
-    const user = req.locals.user
-    const username = user ? user.name : null
-    const entry = new Entry({
-        username: username,
-        title: data.title,
-        body: data.body
-    })
-
-    entry.save((err) => {
-        if(err) return next(err)
-        res.redirect('/')
-    })
-}
+  const data = req.body.entry;
+  const user = res.locals.user;
+  const username = user ? user.name : null;
+  const entry = new Entry({
+    username: username,
+    title: data.title,
+    body: data.body
+  });
+  entry.save((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+};
