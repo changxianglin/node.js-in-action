b3a914b9e526eb8f39bca7f69514b0ac6154a4d6
changxianglin <281532612@qq.com>
2019-02-24 23:59:18 +0800

    use curd teamplate

---
+++ Express_course/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'zhangsan',
+        email: 'zhangsan@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'lisi',
+        email: 'lisi@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'wanger',
+        email: 'wanger@gmail.com',
+        status: 'active'
+    }
+]
+
+module.exports = members
