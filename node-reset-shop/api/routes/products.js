9e76c53d9f8dc7c6e10962ac402c24d2790206d9
changxianglin <281532612@qq.com>
2019-03-04 19:27:38 +0800

    crud auth middleware

--- node-reset-shop/api/routes/products.js
+++ node-reset-shop/api/routes/products.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const multer = require('multer')
+const checkAuth = require('../middleware/check-auth')
+
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads/')
@@ -65,7 +67,7 @@ router.get('/', (req, res, next) => {
         })
 })
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
@@ -75,7 +77,6 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
     product
     .save()
     .then(result => {
-        console.log(result)
         res.status(201).json({
             message: 'Created product successfully',
             createdProduct: {
@@ -126,7 +127,7 @@ router.get('/:productId', (req, res, next) => {
         })
 })
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId
     const updateOps = {}
     for(const ops of req.body) {
@@ -151,7 +152,7 @@ router.patch('/:productId', (req, res, next) => {
         })
 })
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId
     Product.remove({ _id: id })
         .exec()
